ace21c5eae7d4dd25ce1996ddd13da3feb539a32
Yevgeny <unstoo@gmail.com>
2017-02-20 21:07:51 +0300

    update

--- auth.js
+++ auth.js
@@ -23,13 +23,13 @@
   
   
   var JSONP = { 
-    renderFriends: function() {
-      document.addEventListener("change", () => {
-          window.friends.forEach( friend => {
-            console.log(friend.first_name);
-          });
-      });
-    },
+    // renderFriends: function() {
+    //   document.addEventListener("change", () => {
+    //       window.friends.forEach( friend => {
+    //         console.log(friend.first_name);
+    //       });
+    //   });
+    // },
     getFriends: function() {
       var cb = document.createElement('script');
       cb.innerHTML = `function jsonpCb(result) { window.friends = result.response.items }`;
@@ -38,8 +38,7 @@
       var query = `https://api.vk.com/method/friends.get?fields=true&order=random&count=5&v=5.52&access_token=1205819480d97eecb9123072c6d3ff63b9cac2b75cbdc6206e275a1e90e99140c937942870d37784d776b&callback=jsonpCb`
       var script = document.createElement('script');
       script.src = query;
-      document.getElementsByTagName("head")[0].appendChild(script);
-      this.renderFriends();            
+      document.getElementsByTagName("head")[0].appendChild(script);            
     },
   };
   
