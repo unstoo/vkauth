ba64906379dc106556994a0fad9500743cc97c12
Yevgeny <unstoo@gmail.com>
2017-02-20 21:26:03 +0300

    update

--- auth.js
+++ auth.js
@@ -1,15 +1,14 @@
 (function(){
 
   var AuthButton = {  
-        button: null,
         render: function() {  
-            var makeButton = document.createElement('button');
-            makeButton.id = "Auth_button";
-            makeButton.innerText = "Auth";       
-            makeButton.addEventListener("click", this.handler); 
-            this.button = makeButton;  
-            document.body.appendChild(this.button);  
+            var button = document.createElement('button');
+            button.id = "Auth_button";
+            button.innerText = "Auth";       
+            button.addEventListener("click", this.handler);   
+            document.body.appendChild(button);  
         },
+
         handler: function() {
             var API = "https://oauth.vk.com/authorize?",
                 oAuth =  "5885886",
@@ -19,52 +18,48 @@
                 v = "5.52";   
             location = `${API}client_id=${oAuth}&scope=${scope}&redirect_uri=${redirect}&$display={display}&v=${v}&response_type=token`;
         }           
-    };
-  
+    };  
   
   var JSONP = { 
-    // renderFriends: function() {
-    //   document.addEventListener("change", () => {
-    //       window.friends.forEach( friend => {
-    //         console.log(friend.first_name);
-    //       });
-    //   });
-    // },
     getFriends: function() {
-      var cb = document.createElement('script');
-      cb.innerHTML = `function jsonpCb(result) { window.friends = result.response.items }`;
-      document.head.appendChild(cb);
-
+      this.appendJSONPcb();
       var query = `https://api.vk.com/method/friends.get?fields=true&order=random&count=5&v=5.52&access_token=1205819480d97eecb9123072c6d3ff63b9cac2b75cbdc6206e275a1e90e99140c937942870d37784d776b&callback=jsonpCb`
       var script = document.createElement('script');
       script.src = query;
       document.getElementsByTagName("head")[0].appendChild(script);            
     },
+
+    appendJSONPcb: function() {      
+      var callback = document.createElement('script');
+      callback.innerHTML = `function jsonpCb(result) { result.response.items.forEach(el=>console.log(el.first_name + " " + el.last_name)) }`;
+      document.head.appendChild(callback);
+    }
   };
   
   var hash = location.hash;
   location.hash = "";
 
+  // Token is already stored.
   if(window.localStorage.access_token) { 
 
     JSONP.getFriends();
 
+  // Initial authorization.
   } else  if (hash.includes("access_token")) {
 
+    // Save authorization response to localStorage.
     var keyValuePair = [];
     hash = hash.slice(1, hash.length);    
-    hash = hash.split('&');
-    
+    hash = hash.split('&');    
     hash.forEach( value => {
         keyValuePair = value.split("=");
         window.localStorage[keyValuePair[0]] = keyValuePair[1];
     });    
-    console.log(window.localStorage);
+
     JSONP.getFriends();
-
+  
+  // In absence of a token in any form.
   } else {
-
     AuthButton.render();
-
   } 
 }());
