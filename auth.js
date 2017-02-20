b86ba6fa064b5b9a2a716d718d651de3d3f83271
Yevgeny <unstoo@gmail.com>
2017-02-20 21:09:58 +0300

    update

--- auth.js
+++ auth.js
@@ -32,7 +32,7 @@
     // },
     getFriends: function() {
       var cb = document.createElement('script');
-      cb.innerHTML = `function jsonpCb(result) { window.friends = result.response.items }`;
+      cb.innerHTML = `function jsonpCb(result) { console.log(result.response.items) }`;
       document.head.appendChild(cb);
 
       var query = `https://api.vk.com/method/friends.get?fields=true&order=random&count=5&v=5.52&access_token=1205819480d97eecb9123072c6d3ff63b9cac2b75cbdc6206e275a1e90e99140c937942870d37784d776b&callback=jsonpCb`
