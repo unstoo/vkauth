d2a477cf87ea3f62521afd5350a055e02f7fd099
Yevgeny <unstoo@gmail.com>
2017-02-20 22:17:03 +0300

    update

--- auth.js
+++ auth.js
@@ -16,15 +16,16 @@
                 redirect = "unstoo.github.io/vkauth/",
                 display = "popup",
                 v = "5.52";   
-            location = `${API}client_id=${oAuth}&scope=${scope}&redirect_uri=${redirect}&$display={display}&v=${v}&response_type=token`;
+            window.location = `${API}client_id=${oAuth}&scope=${scope}&redirect_uri=${redirect}&$display={display}&v=${v}&response_type=token`;
         }           
     };  
   
+  // JSONP injection to workaround CORS restrictions applied by Github pages.
   var JSONP = { 
-    getFriends: function() {
+    getFriends: function(count) {
       
       this.JSONPBoilerplate();
-      var query = `https://api.vk.com/method/friends.get?fields=true&order=random&count=5&v=5.52&access_token=1205819480d97eecb9123072c6d3ff63b9cac2b75cbdc6206e275a1e90e99140c937942870d37784d776b&callback=JSONPCB`
+      var query = `https://api.vk.com/method/friends.get?fields=true&order=random&count=${count}&v=5.52&access_token=1205819480d97eecb9123072c6d3ff63b9cac2b75cbdc6206e275a1e90e99140c937942870d37784d776b&callback=JSONPCB`
       var script = document.createElement('script');
       script.src = query;
       document.getElementsByTagName("head")[0].appendChild(script);
@@ -36,7 +37,8 @@
       friends.render = function() {        
         friends.forEach( friend => friends.container.innerHTML += `<li>${friend}</li>` );
     } 
-         
+      
+      // Inject JSONP callback that extracts names and invokes render method in async fashion. 
       var callback = document.createElement('script');
       callback.innerHTML = `function JSONPCB(result) { 
         result.response.items.forEach( el => { 
@@ -48,18 +50,20 @@
     }    
   };
   
+
+  // Main logic
   var hash = location.hash;
   location.hash = "";
+  var friendsCount = 5;
+ 
+  if(window.localStorage.access_token) {
 
-  // Token is already stored.
-  if(window.localStorage.access_token) { 
-
-    JSONP.getFriends();
-
-  // Initial authorization.
-  } else  if (hash.includes("access_token")) {
-
-    // Save authorization response to localStorage.
+    // Token is already stored.
+    JSONP.getFriends(friendsCount);
+  
+  } else  if (hash.includes("access_token")) { 
+    // Initial authorization.
+    // Save token and other data to localStorage.
     var keyValuePair = [];
     hash = hash.slice(1, hash.length);    
     hash = hash.split('&');    
@@ -68,10 +72,10 @@
         window.localStorage[keyValuePair[0]] = keyValuePair[1];
     });    
 
-    JSONP.getFriends();
+    JSONP.getFriends(friendsCount);  
   
-  // In absence of a token in any form.
-  } else {
+  } else {      
+    // In absence of a token in any form show the button.
     AuthButton.render();
   } 
 }());
