bc77cc5ea90d6244da96d18e681bc5a3aa57aac6
Yevgeny <unstoo@gmail.com>
2017-02-20 22:40:20 +0300

    update

--- auth.js
+++ auth.js
@@ -11,12 +11,12 @@
 
         handler: function() {
             var API = "https://oauth.vk.com/authorize?",
-                oAuth =  "5885886",
+                app =  "5885886",
                 scope = "wall,friends",
                 redirect = "unstoo.github.io/vkauth/",
                 display = "popup",
                 v = "5.52";   
-            window.location = `${API}client_id=${oAuth}&scope=${scope}&redirect_uri=${redirect}&$display={display}&v=${v}&response_type=token`;
+            window.location = `${API}client_id=${app}&scope=${scope}&redirect_uri=${redirect}&$display={display}&v=${v}&response_type=token`;
         }           
     };  
   
@@ -25,7 +25,12 @@
     getFriends: function(count) {
       
       this.JSONPBoilerplate();
-      var query = `https://api.vk.com/method/friends.get?fields=true&order=random&count=${count}&v=5.52&access_token=1205819480d97eecb9123072c6d3ff63b9cac2b75cbdc6206e275a1e90e99140c937942870d37784d776b&callback=JSONPCB`
+      var queryFriends = `https://api.vk.com/method/friends.get?fields=true&order=random&count=${count}&v=5.52&access_token=1205819480d97eecb9123072c6d3ff63b9cac2b75cbdc6206e275a1e90e99140c937942870d37784d776b&callback=JSONPCBFriends`;
+      var script = document.createElement('script');
+      script.src = query;
+      document.getElementsByTagName("head")[0].appendChild(script);
+      //
+      var query = `https://api.vk.com/method/users.get?user_ids=${localStorage.user_id}&&v=5.52&callback=JSONPCBClient`;
       var script = document.createElement('script');
       script.src = query;
       document.getElementsByTagName("head")[0].appendChild(script);
@@ -36,17 +41,23 @@
       friends.container = document.getElementById('friends');
       friends.render = function() {        
         friends.forEach( friend => friends.container.innerHTML += `<li>${friend}</li>` );
+        friends.container.parentElement.innerHTML = friends.parent;
     } 
       
       // Inject JSONP callback that extracts names and invokes render method in async fashion. 
       var callback = document.createElement('script');
-      callback.innerHTML = `function JSONPCB(result) { 
+      callback.innerHTML = `function JSONPCBFriends(result) { 
         result.response.items.forEach( el => { 
-          friends.push(el.first_name + " " + el.last_name );
+          friends.push(el.first_name + " " + el.last_name);
+        });
+      }`;
+      document.head.appendChild(callback);
+      callback.innerHTML = `function JSONPCBClient(result) { 
+        result.response.items.forEach( el => { 
+          friends.parent(el.first_name + " " + el.last_name);
         });
         friends.render();
       }`;
-      document.head.appendChild(callback);
     }    
   };
   
