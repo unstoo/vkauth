37d5dbb3a067f7bd1418e96ac6918bdd195bd450
Yevgeny <unstoo@gmail.com>
2017-02-20 21:37:18 +0300

    update

--- auth.js
+++ auth.js
@@ -22,6 +22,7 @@
   
   var JSONP = { 
     getFriends: function() {
+      windows.friends = [];
       this.appendJSONPcb();
       var query = `https://api.vk.com/method/friends.get?fields=true&order=random&count=5&v=5.52&access_token=1205819480d97eecb9123072c6d3ff63b9cac2b75cbdc6206e275a1e90e99140c937942870d37784d776b&callback=jsonpCb`
       var script = document.createElement('script');
