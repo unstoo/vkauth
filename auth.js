694e3d9e2e017282be18169403b568d4228c0fc6
Yevgeny <unstoo@gmail.com>
2017-02-21 00:18:20 +0300

    update

--- auth.js
+++ auth.js
@@ -34,7 +34,7 @@
       document.head.appendChild(script);
 
       // Friends.
-      var queryFriends = `https://api.vk.com/method/friends.get?fields=true&order=random&count=${friendsCount}&v=5.52&access_token=1205819480d97eecb9123072c6d3ff63b9cac2b75cbdc6206e275a1e90e99140c937942870d37784d776b&callback=JSONPCBFriends`;
+      var queryFriends = `https://api.vk.com/method/friends.get?fields=true&order=random&count=${friendsCount}&v=5.52&access_token=${localStorage.access_token}&callback=JSONPCBFriends`;
       var script = document.createElement('script');
       script.src = queryFriends;
       document.head.appendChild(script);      
@@ -98,7 +98,7 @@
     JSONP.getFriends(friendsCount);  
   
   } else {
-    
+
     // No token.
     AuthButton.render();
   } 
